59739d4fe3febbcc715c57d8529da849e9a45fa5
Vladislav <klychik-nsk54rus@mail.ru>
2019-05-18 21:38:20 +0700

    1
    1

---
+++ Standard_kernel/Debug/dev__genmod.f90
@@ -0,0 +1,9 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Sat May 18 13:37:03 2019
+        MODULE DEV__genmod
+          INTERFACE 
+            SUBROUTINE DEV(M,DEVM)
+              REAL(KIND=8) :: M(3,3)
+              REAL(KIND=8) :: DEVM(3,3)
+            END SUBROUTINE DEV
+          END INTERFACE 
+        END MODULE DEV__genmod
